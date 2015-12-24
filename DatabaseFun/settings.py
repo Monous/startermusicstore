28ce23156ed42e5a052bb649099bcc472e93fdd7
Moheem Ilyas <monous1380@gmail.com>
2015-12-24 17:48:58 -0500

    try again

--- DatabaseFun/settings.py
+++ DatabaseFun/settings.py
@@ -22,7 +22,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'postgresql-animate-4837',
+        'NAME': 'startermusicstore-database',
         'USER': 'bgsonrjabxwhbw',
         'PASSWORD': 'Dd0O9-zvbMFBDesF9EuUvz81_c',
         'HOST': 'ec2-107-22-197-152.compute-1.amazonaws.com',
